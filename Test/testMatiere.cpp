e2920b11b59fa0d42db8957ebd1bcd8fa266d91a
leonard <lievin.leonard@gmail.com>
2018-01-14 20:20:41 +0100

    ajout test

---
+++ Test/testMatiere.cpp
@@ -0,0 +1,44 @@
+#include "catch.hpp"
+#include "matiere.h"
+
+/*
+TEST_CASE("Verification des accesseurs et de la construction d'une matière", "[matiere]"){
+    matiere m ("codeMatiere", "intitule", 1);
+    SECTION("Accesseur codeMatiere"){
+        REQUIRE(m.codeMatiere == "codeMatiere");
+    }
+    SECTION("Accesseur intitule"){
+        REQUIRE(m.intitule == "intitule");
+    }
+    SECTION("Accesseur coefficient"){
+        REQUIRE(m.codeMatiere == 1);
+    }
+}
+
+TEST_CASE("Verification des modificateurs", "[matiere]"){
+    matiere m ("codeMatiere", "intitule", 1);
+    SECTION("Mods == "code matiere : codeMatiere intitule : intitule coefficient : 1"ificateur codeMatiere"){
+        REQUIRE(m.codeMatiere == "codeMatiere");
+    }
+    SECTION("Modificateur intitule"){
+        REQUIRE(m.intitule == "intitule");
+    }
+    SECTION("Modificateur coefficient"){
+        REQUIRE(m.codeMatiere == 1);
+    }
+}
+
+TEST_CASE("Verification des afficheurs", "[matiere]"){
+	matiere m ("codeMatiere", "intitule", 1);
+	std::string s = "";
+	SECTION("affiche"){
+		m.affiche(s)
+		REQUIRE(s == "code matiere : codeMatiere intitule : intitule coefficient : 1");
+	}
+	SECTION("afficher avec <<"){
+		s = "";
+		s << m;
+		REQUIRE(s == "code matiere : codeMatiere intitule : intitule coefficient : 1");
+	}
+}
+*/
